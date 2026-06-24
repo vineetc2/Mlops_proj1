56d8b90208c47083d62cdbb20d21f400d9b25915
Vineet Kumar <Vineetkumar305@gmail.com>
2026-06-24 20:37:52 +0530

    project structure ready, venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vineet Kumar",
+    author_email="vineetkumar305@gmail.com",
+    packages=find_packages()
+)
